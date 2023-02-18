b1b457c76d6835a4ea9067fbb1b3f9332c1437df
Cairo <cairocampos98@gmail.com>
2023-02-18 19:20:07 -0300

    feat: set theme

--- src/core/composables/useTheme.ts
+++ src/core/composables/useTheme.ts
@@ -1,20 +1,37 @@
-import { ref } from "vue"
+import { ref, watch } from "vue"
 
 type Themes = 'dark' | 'light'
 
 export default function useTheme() {
-  const theme = ref<Themes>('dark')
+  const theme = ref<Themes>('light')
   const toggleTheme = () => {
     theme.value = theme.value === 'dark' ? 'light' : 'dark';
+    handleHtmlAttribute()
+  }
+
+  watch(theme, () => {
+    setThemeOnStorage(),
+    handleHtmlAttribute();
+  })
+
+  const setThemeOnStorage = () => {
+    localStorage.setItem('@Rockstream:theme', theme.value)
+  }
+
+  const handleHtmlAttribute = () => {
+    if(theme.value === 'dark') {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
   }
 
   const setInitialTheme = () => {
-    if(localStorage.getItem('@Rockstream:theme') === 'dark') {
-      document.documentElement.classList.add('dark')
-      return;
+    const storageTheme = localStorage.getItem('@Rockstream:theme') as Themes || theme.value;
+    if(['dark', 'light'].includes(storageTheme)) {
+      theme.value = storageTheme;
     }
-
-    document.documentElement.classList.remove('dark')
+    handleHtmlAttribute();
   }
 
   return {
