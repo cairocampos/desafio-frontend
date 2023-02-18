ad08ed3b9a0164cb2c19beb6bcb2ba86fa9f0303
Cairo <cairocampos98@gmail.com>
2023-02-18 20:37:27 -0300

    feat: extract model search items

--- src/core/models/youtube/search-items.ts
+++ src/core/models/youtube/search-items.ts
@@ -1,23 +1,25 @@
 import { Common } from "./common";
 import { Thumbnail } from "./thumbnail";
 
+export interface VideoInfo {
+  publishedAt: string
+  channelId: string
+  title: string
+  description: string
+  thumbnails: {
+    default: Thumbnail
+    medium: Thumbnail
+    high: Thumbnail
+  }
+  channelTitle: string
+  liveBroadcastContent: string
+  publishTime: string
+}
+
 export interface SearchItems extends Common {
   id: {
     kind: string;
     videoId: string;
   },
-  snippet: {
-    publishedAt: string
-    channelId: string
-    title: string
-    description: string
-    thumbnails: {
-      default: Thumbnail
-      medium: Thumbnail
-      high: Thumbnail
-    }
-    channelTitle: string
-    liveBroadcastContent: string
-    publishTime: string
-  }
+  snippet: VideoInfo
 }
