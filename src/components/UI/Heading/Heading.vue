7470030808d334907a8a37c8b5b5c216b8b662b6
Cairo <cairocampos98@gmail.com>
2023-02-18 20:37:02 -0300

    feat: add text components

---
+++ src/components/UI/Heading/Heading.vue
@@ -0,0 +1,47 @@
+<template>
+  <component :is="isComponent" :class="['heading', `heading__${size}`]">
+    <slot></slot>
+  </component>
+</template>
+
+<script setup lang="ts">
+import { computed, type PropType } from "vue";
+
+type HeadingSize = "lg" | "md" | "sm" | "xs";
+
+const props = defineProps({
+  size: {
+    type: String as PropType<HeadingSize>,
+    default: "md",
+  },
+});
+
+const isComponent = computed(() => {
+  const sizes = {
+    lg: "h1",
+    md: "h2",
+    sm: "h3",
+    xs: "h4",
+  };
+
+  return sizes[props.size];
+});
+</script>
+
+<style scoped lang="scss">
+.heading {
+  @apply text-zinc-600;
+  &__lg {
+    @apply text-2xl leading-8 md:text-5xl md:leading-[4rem] font-bold;
+  }
+  &__md {
+    @apply text-lg leading-6 md:text-2xl md:leading-8 font-bold;
+  }
+  &__sm {
+    @apply text-sm leading-5 md:text-[20px] md:leading-7 font-semibold;
+  }
+  &__xs {
+    @apply text-neutral-100 text-xs leading-5 md:text-[14px] md:leading-6 uppercase;
+  }
+}
+</style>
---
+++ src/components/UI/Text/Text.vue
@@ -0,0 +1,37 @@
+<template>
+  <component :is="tag" :class="[`text text__${size}`]">
+    <slot></slot>
+  </component>
+</template>
+
+<script setup lang="ts">
+import { type PropType } from "vue";
+
+type TextSize = "md" | "sm" | "xs";
+
+defineProps({
+  size: {
+    type: String as PropType<TextSize>,
+    default: "md",
+  },
+  tag: {
+    type: String,
+    default: "p",
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.text {
+  @apply text-zinc-600;
+  &__md {
+    @apply text-lg leading-6 md:text-xl md:leading-8;
+  }
+  &__sm {
+    @apply text-xs leading-5 md:text-base;
+  }
+  &__xs {
+    @apply text-[.725rem] leading-4 md:text-sm md:leading-6;
+  }
+}
+</style>
